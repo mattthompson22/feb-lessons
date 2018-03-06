4d9f92a717a5aa8a22db8062b4802ca21f92ae70
V School <vschool@Vs-Mac-Pro-4.local>
2018-03-05 17:48:57 -0700

    positioning css intro

---
+++ array-methods/main.js
@@ -0,0 +1,11 @@
+var thisStuff = ['cream','donuts', 6, 'cream', 'bananas', 'carrots', 'cream']
+
+var thatStuff = ['shoes', 'socks', 'shirt', 'walkman', 'scarf']
+
+
+
+
+
+console.log(thisStuff.indexOf(6));
+
+// console.log(thisStuff);
